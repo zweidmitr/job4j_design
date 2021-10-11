e72a7dd675565c09700b41043e7d35482802d167
Dmitrii Zwei <zweidmitr@gmail.com>
2021-10-11 14:38:32 +0300

    0. Инструменты [#238814]

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class TriggerTest extends TestCase {
+
+    public void testSomeLogic() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
