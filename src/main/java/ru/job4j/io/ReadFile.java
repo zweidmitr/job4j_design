0443da9d37ef808cf068573b705d9504c682bcfd
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-01 19:47:26 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+            System.out.println("==============");
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(line + "   ln");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
