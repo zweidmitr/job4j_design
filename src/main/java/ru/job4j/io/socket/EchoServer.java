c687f27a3d6f1f7a82e86086cc991f7ca54ef69f
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-08 22:09:56 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/socket/EchoServer.java
+++ src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -35,20 +36,34 @@ public class EchoServer {
                      * В ответ мы записываем строчку.
                      */
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    StringBuilder builder = new StringBuilder();
                     /**
                      * В программе читается весь входной поток.
                      */
                     for (String str = reader.readLine(); str != null && !str.isEmpty(); str = reader.readLine()) {
                         System.out.println("test: " + str);
-                        if (str.contains("Bye")) {
+                        if (str.contains("Exit")) {
                             server.close();
+                            break;
                         }
+                        builder.append(str);
                     }
                     /**
                      * После чтения отправляем ответ окончательно.
                      */
+                    send(out, builder.toString());
                     out.flush();
                 }
+            }
+        }
+    }
+
+    private static void send(OutputStream out, String request) throws IOException {
+        if (request != null && !request.isEmpty()) {
+            if (request.contains("Hello")) {
+                out.write(("Hello, world!!!.\r\n\r\n").getBytes());
+            } else {
+                out.write(("What??\r\n\r\n").getBytes());
             }
         }
     }
