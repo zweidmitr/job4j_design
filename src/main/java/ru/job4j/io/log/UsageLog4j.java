8cf948d6a1f8b7696dcb0b63aa3dfe8532fd115f
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-10 12:54:24 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/log/UsageLog4j.java
+++ src/main/java/ru/job4j/io/log/UsageLog4j.java
@@ -7,11 +7,24 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Dmitrii Zwei";
+        int age = 30;
+
+        byte apartmentNumber = 0;
+        short homeNumber = 1;
+        int index = 999999;
+        long phoneNumber = 8_911_777_7777L;
+
+        float fTemp = 36F;
+        double temp = 0.1;
+        char abracadabra = 'u';
+        boolean isItMagic = false;
+
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("my apartmentNumber = {}, and hoeNumber is {}.", apartmentNumber, homeNumber, phoneNumber);
+        LOG.debug("and your phone is {}, i always forget index {}", phoneNumber, index);
+        LOG.debug("temp on PC show {}, but thermometer show {}", fTemp, temp);
+        LOG.debug("is it magic? {} you say {}...", isItMagic, abracadabra);
 
     }
 }
