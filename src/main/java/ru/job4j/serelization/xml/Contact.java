2875e11f9cfbf70d828371e6405e5b046382d915
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-11 12:02:02 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

---
+++ src/main/java/ru/job4j/serelization/xml/Contact.java
@@ -0,0 +1,24 @@
+package ru.job4j.serelization.xml;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "contact")
+public class Contact {
+    @XmlAttribute
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Contact(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "phone='" + phone + '\''
+                + '}';
+    }
+}
