04165164c616168fd4a061300cbd10b1c3d3813b
Dmitrii Zwei <zweidmitr@gmail.com>
2021-11-15 13:03:36 +0300

    0. Select [#6861 #228556]

--- src/main/java/ru/job4j/sql/create/fauna.sql
+++ src/main/java/ru/job4j/sql/create/fauna.sql
@@ -6,7 +6,8 @@ create table fauna (
 );
 
 insert into fauna(name, avg_age, discovery_date) values ('fish',55,'555-10-02');
-insert into fauna(name, avg_age, discovery_date) values ('fish',7,'520-01-19');
+insert into fauna(name, avg_age, discovery_date) values ('fishCrab',7,'520-01-19');
+insert into fauna(name, avg_age, discovery_date) values ('bigfish',7,'777-01-17');
 
 insert into fauna(name, avg_age, discovery_date) values ('dog',999,'02-01-03');
 insert into fauna(name, avg_age, discovery_date) values ('dog',15,'01-01-01');
@@ -18,7 +19,7 @@ insert into fauna(name, avg_age, discovery_date) values ('homa',10,null);
 select * from fauna;
 
 select * from fauna
-where name LIKE 'fish';
+where name LIKE '%fish%';
 
 select * from fauna
 where avg_age > 10000 and avg_age < 21000;
