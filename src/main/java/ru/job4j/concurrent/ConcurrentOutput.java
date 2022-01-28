463cce7bc62b9f44b5c92769285929c60a4083ac
Dmitrii Zwei <zweidmitr@gmail.com>
2022-01-28 09:00:25 +0300

    concurrent (нужен новый репозиторий)

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        Thread newThread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+        );
+        newThread.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
